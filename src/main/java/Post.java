7eed925bb4cb0f6c266df236cc1a9ccbcfe6298f
Arvo <arvofinland@gmail.com>
2023-03-20 22:51:39 +0300

    add user information

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+import java.text.Normalizer;
+
+public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
